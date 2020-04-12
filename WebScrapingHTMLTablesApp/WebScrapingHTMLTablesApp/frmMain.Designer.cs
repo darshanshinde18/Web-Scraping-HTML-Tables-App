7234a3eaccf66473b9b9a6baa226a05e852fdb08
Darshan Vinayak Shinde <darshanshinde18@gmail.com>
2020-04-12 20:33:10 +0530

    Commiting all files

---
+++ WebScrapingHTMLTablesApp/WebScrapingHTMLTablesApp/frmMain.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WebScrapingHTMLTablesApp
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.btnGetTables = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Url:";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(71, 10);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(537, 20);
+            this.txtUrl.TabIndex = 1;
+            // 
+            // btnGetTables
+            // 
+            this.btnGetTables.Location = new System.Drawing.Point(71, 37);
+            this.btnGetTables.Name = "btnGetTables";
+            this.btnGetTables.Size = new System.Drawing.Size(115, 23);
+            this.btnGetTables.TabIndex = 2;
+            this.btnGetTables.Text = "Process";
+            this.btnGetTables.UseVisualStyleBackColor = true;
+            this.btnGetTables.Click += new System.EventHandler(this.btnGetTables_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(71, 77);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(537, 216);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(620, 305);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnGetTables);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Main Form";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.Button btnGetTables;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ WebScrapingHTMLTablesApp/WebScrapingHTMLTablesApp/frmMain.cs
@@ -0,0 +1,88 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebScrapingHTMLTablesApp
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+
+            txtUrl.Text = "https://www.moneycontrol.com/financials/tataelxsi/ratiosVI/TE";
+        }
+
+        private void btnGetTables_Click(object sender, EventArgs e)
+        {
+            DataSet ds = GetAllTables(txtUrl.Text);
+
+            // Bind data to DataGridView.DataSource  
+            dataGridView1.AutoGenerateColumns = true;
+            dataGridView1.DataSource = ds.Tables[1];
+        }
+
+        private static DataSet GetAllTables(string strRequestUrl)
+        {
+            DataSet ds = new DataSet();
+            try
+            {
+                HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                using (var client = new WebClient())
+                {
+                    string html = client.DownloadString(strRequestUrl);
+                    doc.LoadHtml(html);
+                    foreach (HtmlNode table in doc.DocumentNode.SelectNodes("//table"))
+                    {
+                        try
+                        {
+                            DataTable dt = new DataTable();
+                            int Count = 0;//One time for adding columns
+                            foreach (HtmlNode row in table.SelectNodes("tr"))
+                            {
+                                int tdCount = row.SelectNodes("td").Count;
+                                if (Count.Equals(0))
+                                {
+                                    for (int i = 0; i < tdCount; i++)
+                                    {
+                                        dt.Columns.Add("Column" + i, typeof(string));
+                                    }
+                                }
+                                Count++;
+                                DataRow dr = dt.NewRow();
+                                int tdAddedCount = 0;
+                                foreach (var cell in row.SelectNodes("td"))
+                                {
+                                    if (!(tdAddedCount > tdCount))
+                                    {
+                                        dr["Column" + tdAddedCount] = cell.InnerText;
+                                    }
+                                    tdAddedCount++;
+                                }
+                                dt.Rows.Add(dr);
+                            }
+                            ds.Tables.Add(dt);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex);
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return ds;
+        }
+    }
+}
